b4c54a5867b58f1adf3cdac84e827ec637764e00
Jose Romasanta <jr_r123@hotmail.com>
2016-09-13 14:43:39 -0400

    hjh

--- WebContent/certrefadmin.js
+++ WebContent/certrefadmin.js
@@ -138,11 +138,11 @@ function loadItems(){
 
 function showLoadingMessage()
 {
-	document.getElementById('loadingImage').innerHTML = "Loading data "+"<img height=\"100\" width=\"100\" src=\"images/loading.gif\"></img>";
+	
 }
-function stopLoadingMessage()
+function stopLoadingMessage() 
 {
-	document.getElementById('loadingImage').innerHTML = "";
+	
 }
 
 //showLoadingMessage();
